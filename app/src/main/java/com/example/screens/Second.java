9a39e257ba8dc78350f79e02cce4817dc920bd2c
William Forslund <b19wilfo@student.his.se>
2020-04-21 10:51:53 +0200

    Button now opens and closes second activity.

--- app/src/main/java/com/example/screens/Second.java
+++ app/src/main/java/com/example/screens/Second.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Second extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class Second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView textView = findViewById(R.id.name);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String name = extras.getString("name");
+            textView.setText(name);
+        }
+
+        Button close = findViewById(R.id.close_activity_second);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
